ca839172a614fb71c214a2c271e74a5fc16d9c05
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2021-06-22 17:03:12 -0400

    Update startercode1.js

--- startercode1.js
+++ startercode1.js
@@ -1,45 +1,89 @@
 import * as React from 'react';
-import { StatusBar, View, TextInput } from 'react-native';
+import { StatusBar, View, TextInput, Text, FlatList, TouchableOpacity  } from 'react-native';
 import { LinearGradient } from 'expo';
 
 export default class App extends React.Component {
   state = {
-    inputValue: ''
+    inputValue: '',
+    todos: []
+  };
+
+  changeText = value => {
+    this.setState({
+      inputValue: value
+    });
+  };
+
+  addItem = () => {
+    if (this.state.inputValue !== '') {
+      this.setState(prevState => {
+        const newToDo = {
+          title: this.state.inputValue,
+          createdAt: Date.now(),
+        };
+        
+        var todos = this.state.todos.concat(newToDo);
+
+        this.setState({
+          todos: todos,
+        });
+      });
+    }
   };
 
   render() {
+    const todos = this.state.todos.reverse().map((todo, key) =>
+      <View style={{ flexDirection: 'row', marginTop: 20 }}>
+        <TouchableOpacity style={{
+    width: 20,
+    height: 20,
+    borderRadius: 15,
+    borderWidth: 3,
+    borderColor: 'white',
+    margin: 15
+  }}>
+          </TouchableOpacity>
+        <Text style={{ paddingLeft: 5, marginTop:10, fontSize: 28, color: 'white'}}>{todo.title}</Text>
+      </View>
+    );
+
     return (
-        <LinearGradient colors={['#667eea', '#764ba2']} style={{ flex:1 }}>
-          <StatusBar barStyle="light-content" />
-          <View>
-            <TextInput
-    style={styles.input}
-    placeholder="Type here to add a to do."
-    placeholderTextColor={'#fff'}
-    multiline={true}
-    autoCapitalize="sentences"
-    underlineColorAndroid="transparent"
-    selectionColor={'white'}
-    maxLength={30}
-    returnKeyType="done"
-    autoCorrect={false}
-    blurOnSubmit={true}
-  />
-          </View>
-        </LinearGradient>
+      <LinearGradient colors={['#667eea', '#764ba2']} style={{ flex: 1 }}>
+        <StatusBar barStyle="light-content" />
+        <View>
+          <TextInput
+            style={styles.input}
+            onSubmitEditing={this.addItem}
+            onChangeText={this.changeText}
+            placeholder="Type here to add a to do."
+            value={this.state.inputValue}
+            placeholderTextColor={'#fff'}
+            multiline={true}
+            autoCapitalize="sentences"
+            underlineColorAndroid="transparent"
+            selectionColor={'white'}
+            maxLength={30}
+            returnKeyType="done"
+            autoCorrect={false}
+            blurOnSubmit={true}
+          />
+        </View>
+        <View>
+        {todos}
+        </View>
+      </LinearGradient>
     );
   }
 }
 
 const styles = {
-  input:
-    {
-      marginTop: 30,
+  input: {
+    marginTop: 30,
     paddingTop: 10,
     paddingRight: 15,
     paddingLeft: 15,
     fontSize: 34,
     color: 'white',
-    fontWeight: '500'
-  }
-}
+    fontWeight: '500',
+  },
+};
